7fc336149d469f20a4e7e85191c09f088300ce8a
Jhully Ferreira <jhully.ferreira@cs.up.edu.br>
2025-07-04 00:53:33 -0300

    Add welcome message and style utility for console output

--- src/main/java/br/com/git/musicplayer/Main.java
+++ src/main/java/br/com/git/musicplayer/Main.java
@@ -1,7 +1,31 @@
 package br.com.git.musicplayer;
 
+import br.com.git.musicplayer.util.Style;
+import br.com.git.musicplayer.view.MusicView;
+
+import static java.lang.Thread.sleep;
+
 public class Main {
     public static void main(String[] args) {
+        welcomeMessage();
+        new MusicView().menuMusics();
+    }
 
+    public static void welcomeMessage(){
+        System.out.println(Style.bold + Style.blue + "\n//  M U S I C   P L A Y E R  //\n");
+        System.out.println("---------------------------------\n" + Style.reset);
+
+        try{
+            System.out.println("Inicializando serviços...");
+            sleep(400);
+            System.out.println("▫ Verificando biblioteca de mídias...   " + Style.green + "[ OK ]" + Style.reset);
+            sleep(400);
+            System.out.println("▫ Conectando ao sistema de áudio...      " + Style.green + "[ OK ]" + Style.reset);
+            sleep(400);
+            System.out.println("▫ Calibrando equalizador...              " + Style.green + "[ OK ]\n\n" + Style.reset);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.flush();
     }
 }
---
+++ src/main/java/br/com/git/musicplayer/util/Style.java
@@ -0,0 +1,19 @@
+package br.com.git.musicplayer.util;
+
+public class Style {
+    // Códigos de reset
+    public static final String reset = "\u001B[0m";
+
+    // Cores do texto
+    public static final String black = "\u001B[30m";
+    public static final String red = "\u001B[31m";
+    public static final String green = "\u001B[32m";
+    public static final String yellow = "\u001B[33m";
+    public static final String blue = "\u001B[34m";
+    public static final String purple = "\u001B[35m";
+    public static final String cyan = "\u001B[36m";
+    public static final String white = "\u001B[37m";
+
+    public static String bold = "\u001B[1m";
+
+}
